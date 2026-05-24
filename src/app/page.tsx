6fbef3e2f661bdbf85de2cb784c69cae8adb0486
Iskandar Isabekov <iskandarisabrkov@gmail.com>
2026-05-24 11:23:22 +0500

    Vercel analystics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
 import Dashboard from '@/components/Dashboard';
 import Footer from '@/components/Footer';
+import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
   return (
